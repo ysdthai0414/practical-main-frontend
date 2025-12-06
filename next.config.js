de10cb04f48820b5527455035aa9a7601fbfa5bd
ysdthai0414 <ysdyskmeiji1996@gmail.com>
2025-12-06 20:51:22 +0900

    fix: add standalone output for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
+  output: 'standalone',
+  env: {
         // Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.API_ENDPOINT,
       },
